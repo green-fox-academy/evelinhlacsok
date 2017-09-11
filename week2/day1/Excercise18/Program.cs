c572dfd422777b29783a3a9048bdde61c021cb88
evi <evelinhlacsok@gmail.com>
2017-09-11 22:42:22 +0200

    excercise18

---
+++ week2/day1/Excercise18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise18
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number!");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
