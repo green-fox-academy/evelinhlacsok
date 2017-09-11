2ca3b4bcf113f3fe8a5950d885cc2638d79bfdbe
evi <evelinhlacsok@gmail.com>
2017-09-11 14:24:52 +0200

    excercise5

---
+++ week2/day1/Excercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(22 + 13);
+            Console.WriteLine(13 - 22);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22 / 13.0);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+        }
+    }
+}
