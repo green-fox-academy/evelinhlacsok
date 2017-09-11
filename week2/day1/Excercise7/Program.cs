378b90bd47f88dbcc5415b13938490bef0f5695b
evi <evelinhlacsok@gmail.com>
2017-09-11 14:50:15 +0200

    excercise7

---
+++ week2/day1/Excercise7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 8;
+            Console.WriteLine("My favorite number is " + a);
+        }
+    }
+}
