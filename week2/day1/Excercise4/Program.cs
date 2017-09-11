7ac1323f6cf1cb345348fe6341b226899f074b2a
evi <evelinhlacsok@gmail.com>
2017-09-11 14:01:14 +0200

    excercise4

---
+++ week2/day1/Excercise4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Evi Hlacsok");
+            Console.WriteLine(28);
+            Console.Write("1.62");
+        }
+    }
+}
