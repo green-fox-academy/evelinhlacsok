dfd6cc7bfa4c2b90a99c56fba30c73026c89b3b3
evi <evelinhlacsok@gmail.com>
2017-09-11 23:27:10 +0200

    exercise23

---
+++ week2/day1/Exercise23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise23
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+            Console.WriteLine("I won't cheat on the exam!");
+            }
+        }
+    }
+}
