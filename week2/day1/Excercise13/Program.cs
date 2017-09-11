c4b6b2cf317e1d2491a9983e6d7577299fc9bbcd
evi <evelinhlacsok@gmail.com>
2017-09-11 16:34:34 +0200

    excercise13

---
+++ week2/day1/Excercise13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int currentHours = 16;
+            int currentMinutes = 17;
+            int currentSeconds = 42;
+
+            int totalHours = 23 - currentHours;
+            int totalMinutes = 59 - currentMinutes;
+            int totalSeconds = 59 - currentSeconds;
+
+            Console.WriteLine(totalHours * 3600 + totalMinutes * 60 + totalSeconds);
+
+
+        }
+    }
+}
