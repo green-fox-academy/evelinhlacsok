e1b008a1769dad8734832539469c06c343694955
evi <evelinhlacsok@gmail.com>
2017-09-11 23:51:26 +0200

    exercise24

---
+++ week2/day1/Exercise24/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise24
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+            // Create a program that prints all the even numbers between 0 and 500
+
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                if (i % 2 == 0) 
+                
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
