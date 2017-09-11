7553547a9e8bd07f29d79475b9a3abb39c547f94
evi <evelinhlacsok@gmail.com>
2017-09-11 15:26:47 +0200

    excercise10

---
+++ week2/day1/Excercise10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise10
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Evi";
+            int age = 28;
+            double height = 1.6;
+            bool married = false;
+            
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+        }
+    }
+}
