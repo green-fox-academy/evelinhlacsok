23f0ce885018436e6673ae9597e8627d44eb812e
evi <evelinhlacsok@gmail.com>
2017-09-11 15:15:03 +0200

    excercise7-8

---
+++ week2/day1/Excercise8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int c = a + b;
+
+            a = c - a;
+            b = c - b;
+        }
+    }
+}
---
+++ week2/day1/Excercise9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double massInKg = 52.4;
+            double heightInM = 1.63;
+            
+            Console.WriteLine(massInKg /= heightInM *= heightInM);
+
+        }
+    }
+}
