da0006b935db014ca877b1573ca52d060e8ac2a0
evi <evelinhlacsok@gmail.com>
2017-09-11 16:42:52 +0200

    excercise14

---
+++ week2/day1/Excercise14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise14
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Hello user, what is your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
