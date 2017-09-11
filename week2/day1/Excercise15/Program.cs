0427002fbea2bb6bfee4b53ba3344dd9185e151e
evi <evelinhlacsok@gmail.com>
2017-09-11 17:09:02 +0200

    excercise15

---
+++ week2/day1/Excercise15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise15
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of kilometers, that you would like to convert to miles");
+            int km = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine("That is " + km * 0.621371 + " miles");
+           
+
+
+        }
+    }
+}
