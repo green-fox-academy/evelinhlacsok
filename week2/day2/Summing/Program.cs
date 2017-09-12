4e2462734b9c89426233d12c2acd4cff3631f2bc
evi <evelinhlacsok@gmail.com>
2017-09-12 17:12:54 +0200

    summing

---
+++ week2/day2/Summing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Summing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            
+            int number = 5;
+            
+            Console.WriteLine(Sum(number));
+        }
+
+        public static int Sum(int Input)
+        {
+            for (int i = Input; i > 0; i--)
+            {
+                Input += i - 1;
+               
+            }
+            return Input;
+        }
+    }
+}
