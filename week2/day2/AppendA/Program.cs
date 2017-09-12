a9101b98823a071decbb00145c0a099579789c13
evi <evelinhlacsok@gmail.com>
2017-09-12 16:47:38 +0200

    appenda

---
+++ week2/day2/AppendA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace AppendA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+
+
+            string am = "kuty";
+            
+            Console.WriteLine(appendA(am));
+        }
+
+        public static string appendA(string input)
+        {
+            return input + "a";
+        }
+    }
+}
+
+
