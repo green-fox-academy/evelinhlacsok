ef6249a8c4e4e77b4b4b3cab681e3cfff0ba5f4e
evi <evelinhlacsok@gmail.com>
2017-09-12 17:53:31 +0200

    factorial

---
+++ week2/day2/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+        {
+            int number = 5;
+            
+            Console.WriteLine(factorial(number));
+        }
+
+        public static int factorial(int number)
+        {
+            for (int i = number; i > 1; i--)
+            {
+                number *= (i - 1);
+            }
+            return number;
+        }
+    }
+}
