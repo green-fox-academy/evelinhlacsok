529c6a48f00c6019eec4501d15e2d01d844fdb1a
evi <evelinhlacsok@gmail.com>
2017-09-12 14:01:29 +0200

    doubling

---
+++ week2/day2/Doubling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Doubling
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+            Doubling(ak);
+            Console.WriteLine(ak);
+            
+        }
+
+        public static void Doubling(int ak)
+        {
+            Console.WriteLine(ak*2);
+            Console.ReadLine();
+        }
+    }
+}
