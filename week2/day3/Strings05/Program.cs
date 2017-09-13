8c4e7dcdf998e406dd62259dca7dcb6edfdd50e3
evi <evelinhlacsok@gmail.com>
2017-09-13 15:41:42 +0200

    strings05

---
+++ week2/day3/Strings05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Strings05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            Console.WriteLine(Reverse(str));
+
+            Console.ReadLine();
+
+        }
+
+        public static string Reverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+            int j = chars.Length - 1;
+            for (int i = 0; i < chars.Length/2; i++)
+            {
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                j--;
+            }
+            return new string(chars);
+
+
+        }
+
+    }
+}
