2736f14e32fb06b5220a169178799aa318380b25
evi <evelinhlacsok@gmail.com>
2017-09-13 11:55:05 +0200

    correctfolder

--- week2/day3/Strings01/Program.cs
+++ week2/day3/Strings01/Program.cs
@@ -8,15 +8,15 @@ namespace Strings01
         public static void Main(string[] args)
         {
             string example = "In a dishwasher far far away";
+            example = example.Replace("dishwasher", "galaxy");
             
-
-            Console.WriteLine(example.Replace("dishwasher", "galaxy"));
+            
 
             // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
             // Please fix it for me!
             // Expected ouput: In a galaxy far far away
 
-            //Console.WriteLine(example);
+            Console.WriteLine(example);
         }
     }
 }
---
+++ week2/day3/Strings02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://www.reddit.com/r/nevertellmethebots";
+            url = url.Replace("bots", "odds");
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crutial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            Console.WriteLine(url);
+        }
+    }
+}
