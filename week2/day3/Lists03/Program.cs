7d6e22479c3bf08439f699f6250c7f3382aec359
evi <evelinhlacsok@gmail.com>
2017-09-13 16:58:34 +0200

    lists03

---
+++ week2/day3/Lists03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        var far = new List<string> {"kuty", "macsk", "kacs", "rók", "halacsk"};
+                    // Add "a" to every string in the far list.
+            foreach (var fullname in far)
+            {
+                Console.WriteLine(fullname + "a");
+            }
+            
+                    
+        }
+    }
+}
