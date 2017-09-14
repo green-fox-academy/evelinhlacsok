64fb4d2a4ae9d6621fe1d7c55e250a24fd1c1faa
evi <evelinhlacsok@gmail.com>
2017-09-14 17:28:13 +0200

    DrawTriangle

---
+++ week2/day4/day01-28-DrawTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day01_28_DrawTriangle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+            
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            string sign = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(sign = "*" + sign);
+            }
+
+        }
+    }
+}
