7f547c0cc3ec79953dc775a8442294b34ad4ba1f
evi <evelinhlacsok@gmail.com>
2017-09-14 11:19:32 +0200

    cuboid

---
+++ week2/day4/Lists07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists07
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            
+            
+                string swap = list[2];
+                list[2] = list[5];
+                list[5] = swap;
+
+            foreach (var quote in list)
+            {
+                Console.Write(quote + " ");
+            }
+            
+            
+            
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+            
+            // What I cannot create, I do not understand
+            // swap cannot and do
+        }
+    }
+}
---
+++ week2/day4/day01-12-cuboid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day01_12_cuboid
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double slide1 = 5;
+            double slide2 = 5;
+            double slide3 = 5;
+            
+            Console.WriteLine("Surface Area: " + 6 * slide1 * slide2 + " m2");
+            Console.WriteLine("Volume: " + slide1 * slide2 * slide3 + " m3");
+
+        }
+    }
+}
