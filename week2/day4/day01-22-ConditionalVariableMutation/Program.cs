0801820ba6467f4ef433644b9a790bbc18731199
evi <evelinhlacsok@gmail.com>
2017-09-14 15:10:21 +0200

    VariableMutation

--- week2/day4/day01-22-ConditionalVariableMutation/Program.cs
+++ week2/day4/day01-22-ConditionalVariableMutation/Program.cs
@@ -47,6 +47,19 @@ namespace day01_22_ConditionalVariableMutation
             // and isBonus is false decrement c by 1
             // if isBonus is true c should remain the same
 
+            if ((credits >= 50) && (isBonus == false))
+            {
+                c -= 2;
+            }
+            else if ((credits < 50) && (isBonus == false))
+            {
+                c -= 1;
+            }
+            else if (isBonus == true)
+            {
+                c = c;
+            }
+          
             Console.WriteLine(c);
 
             int d = 8;
@@ -58,6 +71,19 @@ namespace day01_22_ConditionalVariableMutation
             // if time is more than 200
             // set out3 to "Time out"
             // otherwise set out3 to "Run Forest Run!"
+
+            if ((d % 4 == 0) && (time < 200))
+            {
+                out3 = "check";
+            }
+            else if (time > 200)
+            {
+                out3 = "Time out";
+            }
+            else
+            {
+                out3 = "Run Forest Run!";
+            }
 
             Console.WriteLine(out3);
         }
