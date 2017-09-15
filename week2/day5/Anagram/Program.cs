ad3b022a89616a26849b3d6abf103b1082797f1a
evi <evelinhlacsok@gmail.com>
2017-09-15 11:45:34 +0200

    Anagram-start

---
+++ week2/day5/Anagram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anagram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+         //Create a function named is anagram following your current language's style guide. 
+         //It should take two strings and return a boolean value depending on whether its an anagram or not.
+            
+            Console.WriteLine("The first word is: ");
+            string firstword = Console.ReadLine();
+            
+            Console.WriteLine("The second word is: ");
+            string secondword = Console.ReadLine();
+            
+            Console.WriteLine(Anagram(firstword, secondword));
+        }
+
+        public static string Anagram(string word1, string word2)
+        {
+            if (word1.Length != word2.Length)
+            {
+                
+            }
+        }
+    }
+}
