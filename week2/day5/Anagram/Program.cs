c20ba8972e417a73086955e8c65dc1660c913626
evi <evelinhlacsok@gmail.com>
2017-09-15 13:20:41 +0200

    anagrama

--- week2/day5/Anagram/Program.cs
+++ week2/day5/Anagram/Program.cs
@@ -13,19 +13,36 @@ namespace Anagram
             
             Console.WriteLine("The first word is: ");
             string firstword = Console.ReadLine();
+            firstword = firstword.ToLower();
             
             Console.WriteLine("The second word is: ");
             string secondword = Console.ReadLine();
+            secondword = secondword.ToLower();
             
-            Console.WriteLine(Anagram(firstword, secondword));
+            Console.WriteLine("Your senctence is an anagrama: " + Anagram(firstword, secondword));
+
+            Console.ReadLine();
         }
 
-        public static string Anagram(string word1, string word2)
+        public static bool Anagram(string word1, string word2)
         {
-            if (word1.Length != word2.Length)
+            
+            char[] array1 = word1.ToCharArray();
+            char[] array2 = word2.ToCharArray();
+            
+            Array.Sort(array1);
+            Array.Sort(array2);
+            
+            string newstring1 = new string(array1);
+            string newstring2 = new string(array2);
+            
+            if (newstring1 == newstring2)
             {
-                
+                return true;
             }
+            return false;
+
+           
         }
     }
 }
