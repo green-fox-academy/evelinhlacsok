24f9910aa89a38c21034327449736b9e764803f7
evi <evelinhlacsok@gmail.com>
2017-09-17 23:39:19 +0200

    DoubleItems

---
+++ week2/day6/day02-DoubleItems/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day02_DoubleItems
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            int[] ag = {3, 4, 5, 6, 7};
+            for (int i = 0; i < ag.GetLength(0); i++)
+            {
+                ag [i] *= 2;
+            }
+
+            foreach (var number in ag)
+            {
+                Console.Write(number + ", ");
+            }
+            
+        }
+    }
+}
