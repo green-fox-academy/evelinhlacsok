89303c83d32c6f201098665bfe0f33a7685c120b
evi <evelinhlacsok@gmail.com>
2017-09-17 23:19:29 +0200

    PrintAll

---
+++ week2/day6/day02-PrintAll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day02_PrintAll
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+
+            int[] af = {4, 5, 6, 7};
+            foreach (var number in af)
+            {
+                Console.Write(number + ", ");
+            }
+        }
+    }
+}
