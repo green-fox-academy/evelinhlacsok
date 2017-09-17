1e574a4303c9159099c1ecc8cc781440d3ba5599
evi <evelinhlacsok@gmail.com>
2017-09-17 15:12:45 +0200

    GuessTheNumber

---
+++ week2/day6/day01-29-DrawPyramid/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day01_29_DrawPyramid
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            Console.WriteLine("how big the pyramid should be? give me a number!");
+            int number = int.Parse(Console.ReadLine());
+            string sign = " ";
+            string space = " ";
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j < number-i+1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+               
+            {
+                /* for (int j = 0; j < number - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < number +1 ; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int l = 0; l < number + i; l++)
+                {
+                    Console.Write(" ");
+                }
+                
+                Console.WriteLine(); */
+            }
+        }
+    }
+}
---
+++ week2/day6/day01-33-GuessTheNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day01_33_GuessTheNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+
+
+            int number = 7;
+            
+            Console.WriteLine("guess the number!");
+            int guess = 1;
+
+            while (guess != number)
+            {
+                guess = int.Parse(Console.ReadLine());
+                if (number < guess)
+                {
+                    Console.WriteLine("The stored number is lower");
+                }
+                else if (number > guess)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+                Console.WriteLine("Try to guess again!");
+            }
+            
+            if (guess == number)
+            {
+                Console.WriteLine("You found the number: 7");
+            }
+
+        }
+    }
+}
