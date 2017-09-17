2a4c598c2faaa0eee34b7d5a3c40d580af16cd6a
evi <evelinhlacsok@gmail.com>
2017-09-17 23:30:47 +0200

    DiagonalMatrix

---
+++ week2/day6/day02-DiagonalMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day02_DiagonalMatrix
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            int[,] matrix = new int[4,4];
+            
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
