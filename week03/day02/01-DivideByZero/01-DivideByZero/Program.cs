2d8692c047d8f041d42366b319f5a070bf0c41c9
evi <evelinhlacsok@gmail.com>
2017-09-19 12:09:44 +0200

    dividebyzero

---
+++ week03/day02/01-DivideByZero/01-DivideByZero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+
+            Console.WriteLine("Give me a number, so I can divide ten with it!");
+            int input = int.Parse(Console.ReadLine());
+            int number = 10;
+			DivideByZero(number, input);
+         
+
+        }
+        public static int DivideByZero(int number, int input)
+        {
+            int result = 0;
+			try
+			{
+				result = number / input;
+				Console.WriteLine(result);
+			}
+			catch (DivideByZeroException e)
+			{
+				Console.WriteLine("Cant divide by zero!");
+			}
+            return result;
+        }
+       
+    }
+}
