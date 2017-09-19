380ecad58f3d3c98c520efce8c5bacc7311736c8
evi <evelinhlacsok@gmail.com>
2017-09-19 23:04:34 +0200

    WriteMultipleLines

---
+++ week03/day02/05-WriteMultipleLines/05-WriteMultipleLines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			// Create a function that takes 3 parameters: a path, a word and a number,
+			// than it should write to a file.
+			// The path parameter should be a string, that describes the location of the file.
+			// The word parameter should be a string, that will be written to the file as lines
+			// The number paramter should describe how many lines the file should have.
+			// So if the word is "apple" and the number is 5, than it should write 5 lines
+			// to the file and each line should be "apple"
+			// The function should not raise any error if it could not write the file.
+
+            string path = @"./lorem-ipsum.txt";
+            string word = "succulents";
+            int number = 8;
+            MultipleLines(path, word, number);
+           // Console.ReadLine();
+
+		}
+        public static void MultipleLines (string path, string word, int number)
+        {
+            using (StreamWriter writer = new StreamWriter(path)) 
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    writer.WriteLine(word);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file");
+            }
+        }
+    }
+}
