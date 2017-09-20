ca2a06761622dd2c3b8796937fcacad79a1ef75d
evi <evelinhlacsok@gmail.com>
2017-09-20 15:50:58 +0200

    FillWithRectangles

---
+++ week03/day03/07-FillWIthRectangles/07-FillWIthRectangles/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace _07_FillWIthRectangles
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { 
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw four different size and color rectangles.
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawRectangle(10, 10, 150, 50);
+
+            foxDraw.FillColor(Colors.Magenta);
+            foxDraw.DrawRectangle(50, 30, 300, 80);
+
+            foxDraw.FillColor(Colors.Gold);
+            foxDraw.DrawRectangle(230, 70, 250, 50);
+
+            foxDraw.FillColor(Colors.MediumAquamarine);
+            foxDraw.DrawRectangle(180, 200, 180, 280);
+        }
+    }
+}
