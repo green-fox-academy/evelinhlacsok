41923d619812179e121e2398edfd299ac302a530
evi <evelinhlacsok@gmail.com>
2017-09-20 15:35:31 +0200

    CenteredSquare

---
+++ week03/day03/06-CenteredSquare/06-CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace _06_CenteredSquare
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // draw a green 10x10 square to the canvas' center.
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.FillColor(Colors.Gold);
+            foxDraw.DrawRectangle(canvas.Width / 2 - 50, canvas.Height / 2 - 50, 50, 50);
+        }
+    }
+}
