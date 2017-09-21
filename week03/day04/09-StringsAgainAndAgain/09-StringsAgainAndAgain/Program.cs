ddad862a33fa158397c5315e03b4b74a203c9e32
evi <evelinhlacsok@gmail.com>
2017-09-21 21:40:17 +0200

    StringsAgainAndAgain

---
+++ week03/day04/09-StringsAgainAndAgain/09-StringsAgainAndAgain/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_StringsAgainAndAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+            Console.WriteLine(StarSeparator("plants are friends honey"));
+            Console.ReadLine();
+        }
+
+        private static string StarSeparator(string input)
+        {
+            string result;
+            int inputlength = input.Length;
+            if (inputlength == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (input[0] == ' ')
+                {
+                    result = "*";
+                    return result + StarSeparator(input.Substring(1));
+                }
+                else
+                {
+                    return input[0] + StarSeparator(input.Substring(1));
+                }
+            }
+    
+        }
+    }
+}
