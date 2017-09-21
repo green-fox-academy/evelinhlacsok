c64adffb9a64c774b5c30bf56b6508c954fd8263
evi <evelinhlacsok@gmail.com>
2017-09-21 11:41:02 +0200

    SumDigit

---
+++ week03/day04/03-SumDigit/03-SumDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+            // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+            // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+            Console.WriteLine(SumDigit(8888));
+            Console.ReadLine();
+        }
+        public static int SumDigit (int n)
+        {
+            if (n == 0)
+                return 0;
+            return (n % 10) + SumDigit (n / 10);
+        }
+    }
+}
