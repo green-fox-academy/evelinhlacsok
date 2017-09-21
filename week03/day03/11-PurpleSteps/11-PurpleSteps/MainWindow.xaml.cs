400d04f06422ec75c31841955d18b31f9df9e657
evi <evelinhlacsok@gmail.com>
2017-09-21 23:24:07 +0200

    PurpleSteps

---
+++ week03/day03/11-PurpleSteps/11-PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+
+namespace _11_PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            double x = 20;
+            double y = 20;
+
+            for (int i = 0; x < canvas.Width / 3 * 2; i++)
+            {
+               for (int j = 0; y < canvas.Height / 3 * 2; i++)
+               {
+                    foxDraw.FillColor(Colors.Purple);
+                    foxDraw.StrokeColor(Colors.Black);
+                    foxDraw.DrawRectangle(x, y, 20, 20);
+                    x += 20;
+                    y += 20;
+                }
+            }
+        }
+    }
+}
