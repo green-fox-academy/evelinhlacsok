19a8b47c936d8bca1f3d7e23b2259275a4e09ed9
evi <evelinhlacsok@gmail.com>
2017-09-21 10:18:30 +0200

    Counter

---
+++ week03/day03/10-RainbowBoxes/10-RainbowBoxes/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace _10_RainbowBoxes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 2 parameters:
+            // the square size, and the fill color,
+            // and draws a square of that size and color to the center of the canvas.
+            // create a loop that fills the canvas with rainbow colored squares.
+
+            DrawSquare(foxDraw, 250, Colors.Magenta);
+            DrawSquare(foxDraw, 150, Colors.Gold);
+            DrawSquare(foxDraw, 60, Colors.Aquamarine);
+
+        }
+        private void DrawSquare (FoxDraw squarez, int size, Color color)
+        {
+            squarez.FillColor(color);
+            squarez.DrawRectangle((canvas.Width-size)/2, (canvas.Height-size)/2, size, size);
+
+        }
+    }
+}
---
+++ week03/day04/01-Counter/01-Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            CounterWithRecursion(8);
+            Console.ReadLine();
+            
+        }
+        public static void CounterWithRecursion (int n)
+        {
+            if (n < 0)
+                return;
+            Console.WriteLine(n);
+            CounterWithRecursion(n - 1);
+
+        }
+    }
+}
