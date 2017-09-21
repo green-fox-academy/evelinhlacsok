9e90a6b5c096e62c96d092058acf6f62ffb43852
evi <evelinhlacsok@gmail.com>
2017-09-21 10:44:49 +0200

    NumberAdder

---
+++ week03/day04/02-NumberAdder/02-NumberAdder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+            Console.WriteLine(NumberAdder(9));
+            Console.ReadLine();
+        }
+        private static int NumberAdder (int n)
+        {
+            if (n<0)
+                return 0;
+            return n + NumberAdder(n - 1);
+        }
+      //  private static int  NumberAdder (int n)
+       // {
+        //    if (n == 1)
+       //         return 1;
+        //    return n + NumberAdder(n - 1);
+       // }
+    }
+}
