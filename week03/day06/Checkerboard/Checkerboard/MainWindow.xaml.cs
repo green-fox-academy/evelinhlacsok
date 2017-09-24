bb2ec5e654364a32f618286e790c81e60c79fcf3
evi <evelinhlacsok@gmail.com>
2017-09-24 22:30:26 +0200

    checkerboard

---
+++ week03/day06/Checkerboard/Checkerboard/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+
+namespace Checkerboard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // fill the canvas with a checkerboard pattern.
+
+            foxDraw.BackgroundColor(Colors.Gold);
+
+            double size = 75;
+            double x = 0;
+            double y = 0;
+            //Squares(foxDraw, a, b);
+            //Squares(foxDraw, a+75, b);
+            //Squares(foxDraw, a, b+75);
+            //Squares(foxDraw, a+75, b+75);
+
+            for (int i = 0; i < 8; i++)
+            {
+                    if (i % 2 == 1)
+                    {
+                         x += size;
+                    }
+                for (int j = 0; j < 4; j++)
+                {
+                    foxDraw.FillColor(Colors.Black);
+                    foxDraw.DrawRectangle(x, y, size, size);
+                    x += 2 * size;
+                }
+                x = 0;
+                y += size;
+            }
+
+        }
+    }
+}
