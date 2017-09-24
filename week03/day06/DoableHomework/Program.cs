1088434df6c5487a116d5fca987ccf4c5d133c32
evi <evelinhlacsok@gmail.com>
2017-09-24 23:53:42 +0200

    doableHomework

---
+++ week03/day06/DoableHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoableHomework
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            /*Create 5 trees
+                Store the data of them in variables in your program
+            for every tree the program should store its'
+            type
+            leaf color
+            age
+            sex
+                you can use just variables, or lists and/or maps */
+            
+            string tree1 = "pine, silver, 3, male";
+            string tree2 = "beech, green, 10, female";
+            string tree3 = "oak, yellow, 15, male";
+            string tree4 = "cedar, green, 20, male";
+            string tree5 = "yew, grey, 8, female";
+            
+            List<string> TreeList = new List<string>(){tree1, tree2, tree3, tree4, tree5};
+
+       
+
+
+
+        }
+    }
+}
