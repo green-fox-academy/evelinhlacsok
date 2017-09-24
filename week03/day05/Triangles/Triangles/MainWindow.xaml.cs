1f849c44feeb8e1c2a46a8ce2ec8fb9da80d8dca
evi <evelinhlacsok@gmail.com>
2017-09-24 12:48:53 +0200

    trying to color it

--- week03/day05/Triangles/Triangles/MainWindow.xaml.cs
+++ week03/day05/Triangles/Triangles/MainWindow.xaml.cs
@@ -14,32 +14,42 @@ namespace Triangles
             foxDraw.BackgroundColor(Colors.Gold);
             int level = 5;
             int size = 600;
+            Random random = new Random();
 
-            DrawTriangles(foxDraw, size, 0, 0, level);
-          //  DrawTriangles.DrawLine(foxDraw, size, 0, size / 2, size);
-          //  DrawTriangles.DrawLine(foxDraw, size / 2, size, 0, 0);
+            DrawTriangles(foxDraw, size, 0, 0, level, RandomColor(random));
+            //  DrawTriangles.DrawLine(foxDraw, size, 0, size / 2, size);
+            //  DrawTriangles.DrawLine(foxDraw, size / 2, size, 0, 0);
 
 
         }
-        private void DrawTriangles(FoxDraw triangles, int size, int x, int y, int level)
+        public Color RandomColor(Random random)
+        {
+            return Color.FromArgb((byte)random.Next(255),
+                                 (byte)random.Next(255),
+                                 (byte)random.Next(255),
+                                 (byte)random.Next(255));
+        }
+        private void DrawTriangles(FoxDraw triangles, int size, int x, int y, int level, Color color)
         {
             if (level > 0)
             {
+                triangles.StrokeColor(Colors.BlueViolet);
                 triangles.DrawLine(x + size / 2, y, x + size / 4 * 3, y + size / 2);
                 triangles.DrawLine(x + size / 4 * 3, y + size / 2, x + size / 4, y + size / 2);
                 triangles.DrawLine(x + size / 4, y + size / 2, x + size / 2, y);
 
                 // TODO: nagy haromszog
 
-                //triangles.DrawLine(x, y, size, y);
-                //triangles.DrawLine(size, y, x + size / 2, y+size);
-                //triangles.DrawLine(x + size / 2, y+size, x, y);
+                triangles.DrawLine(x, y, x + size, y);
+                triangles.DrawLine(x + size, y, x + size / 2, y + size);
+                triangles.DrawLine(x + size / 2, y + size, x, y);
 
-                DrawTriangles(triangles, size / 2, x, y, level - 1);
-                DrawTriangles(triangles, size / 2, x + size / 2, y, level - 1);
-                DrawTriangles(triangles, size / 2, x + size / 4, y + size / 2, level -1);
+                DrawTriangles(triangles, size / 2, x, y, level - 1, color);
+                DrawTriangles(triangles, size / 2, x + size / 2, y, level - 1, color);
+                DrawTriangles(triangles, size / 2, x + size / 4, y + size / 2, level - 1, color);
 
             }
+
         }
     }
 }
