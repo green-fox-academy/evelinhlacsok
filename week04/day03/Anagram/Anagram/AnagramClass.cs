504b99a258b5143fdd3a253eb3d32aa1529bc44c
evi <evelinhlacsok@gmail.com>
2017-09-27 15:22:28 +0200

    Test1

---
+++ week04/day03/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool IsItAnagram(string firstword, string secondword)
+        {
+            //making it lowercase
+            firstword = firstword.ToLower();
+            secondword = secondword.ToLower();
+
+            //making the arrays from the strings 
+            char[] array1 = firstword.ToCharArray();
+            char[] array2 = secondword.ToCharArray();
+
+            //sorting the arrays
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            //turning arrays to strings
+            string sorted_firstword = new string(array1);
+            string sorted_secondword = new string(array2);
+
+            if (sorted_firstword == sorted_secondword)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ week04/day03/Anagram/AnagramTest/AnagramTestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+using NUnit.Framework;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTestClass
+    {
+        AnagramClass myOriginal = new AnagramClass();
+
+        [Test]
+        public void IsItAnagram ()
+        {
+           string firstword = "keret";
+           string secondword = "retek";
+
+           bool output = true;
+
+            Assert.AreEqual(output, myOriginal.IsItAnagram(firstword, secondword));
+        }
+    }
+}
