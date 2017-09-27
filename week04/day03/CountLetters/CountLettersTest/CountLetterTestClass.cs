cf5309aeb20e27df177bdcd33b7e6b47d4dc5e08
evi <evelinhlacsok@gmail.com>
2017-09-27 16:02:35 +0200

    Countletters-basicsettings

---
+++ week04/day03/CountLetters/CountLettersTest/CountLetterTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetters;
+using NUnit.Framework;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CountLetterTestClass
+    {
+        CountLetterClass myOriginal = new CountLetterClass();
+
+        [Test]
+        public void 
+    }
+}
