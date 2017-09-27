ebe6f49a1f2672dad3ce84e3cc8d1ff46cce1f4c
evi <evelinhlacsok@gmail.com>
2017-09-27 13:49:14 +0200

    Sum-test-start

---
+++ week04/day03/Sum/Sum/SumClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sum
+{
+    public class SumClass
+    {
+
+        public int ListOfIntegers(List<int> inputList)
+        {
+            int numbers = 0;
+            return numbers;
+        }
+    }
+}
---
+++ week04/day03/Sum/SumTest/SumTestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+using NUnit.Framework;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTestClass
+    {
+        SumClass myOriginal = new SumClass();
+
+        [Test]
+        public void listOfIntegers ()
+        {
+            //arrange
+            List<int> inputList = new List<int> { 1, 2, 3, 4 };
+
+            //act
+            int output = 10;
+
+            //assert
+            Assert.AreEqual(output, myOriginal.ListOfIntegers(inputList));
+        }
+
+       
+    }
+}
