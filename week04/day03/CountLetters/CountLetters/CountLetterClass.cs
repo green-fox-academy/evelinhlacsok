48ed477030a244a9a32f12297db3b607a517d44b
evi <evelinhlacsok@gmail.com>
2017-09-27 16:49:28 +0200

    function is done

--- week04/day03/CountLetters/CountLetters/CountLetterClass.cs
+++ week04/day03/CountLetters/CountLetters/CountLetterClass.cs
@@ -8,6 +8,22 @@ namespace CountLetters
 {
     public class CountLetterClass
     {
-        public string
+        public Dictionary<char, int>MyLetterCounter(string input)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (dictionary.ContainsKey(letter))
+                {
+                    dictionary[letter]++;
+                }
+                else
+                {
+                    dictionary.Add(letter, 1);
+                }
+            }
+            return dictionary;
+        }
     }
 }
