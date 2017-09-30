8e820adaf3f5e6f3222865e461d04ebf45294b46
evi <evelinhlacsok@gmail.com>
2017-09-30 22:47:32 +0200

    printer

---
+++ week04/day06_practice/week02-day02-Printer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace week02_day02_Printer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+
+            string[] parameter = {"pal", "kata", "peter", "joreggelt"};
+            Printer(parameter);
+        }
+
+        public static void Printer(string[] parameter)
+        {
+            for (int i = 0; i < parameter.Length; i++)
+            {
+                Console.WriteLine(parameter[i]);
+            }
+        }
+    }
+}
