61b05ab4e7cb8d116ae569e982d5a0d08cc4ecee
evi <evelinhlacsok@gmail.com>
2017-09-30 22:54:53 +0200

    third

---
+++ week04/day06_practice/week02-day02-Third/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week02_day02_Third
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+            
+            int[] q = {4, 5, 6, 7};
+
+            Console.WriteLine(q[2]);
+        }
+    }
+}
