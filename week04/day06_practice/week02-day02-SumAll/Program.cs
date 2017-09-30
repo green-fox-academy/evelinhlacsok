2e3da72b94dbe1acc993b6e0284ac86a0d06a84b
evi <evelinhlacsok@gmail.com>
2017-10-01 00:01:47 +0200

    sumAll

---
+++ week04/day06_practice/week02-day02-SumAll/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace week02_day02_SumAll
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            
+            int[] ai = new int[] {3, 4, 5, 6, 7};
+            int sum = 0;
+            for (int i = 0; i < ai.Length; i++)
+            {
+                sum += ai[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
