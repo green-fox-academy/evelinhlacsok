d273bfb746fb6e8aa81d5f55fb27b9feb8458630
evi <evelinhlacsok@gmail.com>
2017-09-30 15:47:38 +0200

    fixing countfromto exercise

--- week04/day06_practice/loops/Program.cs
+++ week04/day06_practice/loops/Program.cs
@@ -1,27 +1,16 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Runtime.ExceptionServices;
+using System.Globalization;
+using System.Security;
 
-namespace day01_26_CountFromTo
+namespace loops
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            // Create a program that asks for two numbers
-            // If the second number is not bigger than the first one it should print:
-            // "The second number should be bigger"
-            //
-            // If it is bigger it should count from the first number to the second by one
-            //
-            // example:
-            //
-            // first number: 3, second number: 6, should print:
-            //
-            // 3
-            // 4
-            // 5
-            
+
             Console.WriteLine("Give me the first number");
             int firstnumber = int.Parse(Console.ReadLine());
 
