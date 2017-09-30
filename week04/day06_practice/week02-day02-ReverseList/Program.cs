e297b98b68122edfb44e9d840d039b6cc2ad083d
evi <evelinhlacsok@gmail.com>
2017-10-01 00:13:22 +0200

    reverseList

---
+++ week04/day06_practice/week02-day02-ReverseList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week02_day02_ReverseList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            
+            int[] ai = new int[]{3, 4, 5, 6, 7};
+            Array.Reverse(ai);
+
+            foreach (var element in ai)
+            {
+                Console.Write(element + ", ");
+            }
+        }
+    }
+}
