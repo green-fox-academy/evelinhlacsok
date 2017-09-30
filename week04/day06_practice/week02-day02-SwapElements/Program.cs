8c8f73738ace47c16e87edcd1a27d4f9b5a85033
evi <evelinhlacsok@gmail.com>
2017-09-30 23:54:21 +0200

    appenda and swap elements

---
+++ week04/day06_practice/week02-day02-SwapElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week02_day02_SwapElements
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            
+            string[] abc = new string[]{"first", "second", "third"};
+
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            foreach (var element in abc)
+            {
+                Console.Write(element + ", ");
+            }
+        }
+    }
+}
---
+++ week04/day06_practice/week02-day02-appendA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week02_day02_appendA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            string[]nimals = new string[]{"kuty", "macsk", "cic"};
+            appendA(nimals);
+        }
+
+        public static void appendA(string[] nimals)
+        {
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                Console.Write(nimals[i] + "a" + ", ");
+            }
+        }
+    }
+}
