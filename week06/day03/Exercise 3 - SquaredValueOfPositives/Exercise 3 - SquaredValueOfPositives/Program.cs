17253d6e9109f2bc242dff254c326479c67cebb7
evi <evelinhlacsok@gmail.com>
2017-10-18 13:03:53 +0200

    positive squares with query

---
+++ week06/day03/Exercise 3 - SquaredValueOfPositives/Exercise 3 - SquaredValueOfPositives/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3___SquaredValueOfPositives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> squareWithQuery = from square in n
+                                               where square > 0
+                                               select square * square;
+
+            foreach (var number in squareWithQuery)
+            {
+            Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
