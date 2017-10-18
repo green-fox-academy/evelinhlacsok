7594e62ceb73670d76664bd85a87717cd827682d
evi <evelinhlacsok@gmail.com>
2017-10-18 16:50:51 +0200

    char array to string with lambda

---
+++ week06/day03/Exercise9 - CharArrayToString/Exercise9 - CharArrayToString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9___CharArrayToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to convert a char array to a string!
+
+            char[] input = { 'd', 'o', 'g', 'g', 'o' };
+
+            var toStringWithLambda = String.Join(".", input.Select(x => x));
+            Console.WriteLine(toStringWithLambda);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
