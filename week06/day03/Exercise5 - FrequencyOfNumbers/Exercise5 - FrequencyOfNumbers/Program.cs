8f2f18e0d8e81ae17c2acaf9d0d78db3983e3377
evi <evelinhlacsok@gmail.com>
2017-10-18 15:00:39 +0200

    frequency of numbers both with query and lambda

---
+++ week06/day03/Exercise5 - FrequencyOfNumbers/Exercise5 - FrequencyOfNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5___FrequencyOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyWithQuery = from freq in n
+                                                  orderby freq ascending
+                                                  group freq by freq into UniqNum
+                                                  select new
+                                                  {
+                                                      UniqNum.Key,
+                                                      Count = (from freq in UniqNum select freq).Count()
+                                                  };
+
+            foreach (var number in frequencyWithQuery)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+
+            var frenquencyWithLambda = n.OrderBy(y => y).GroupBy(y => y).Select(y => y.Count());
+
+            foreach (var number in frenquencyWithLambda)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
