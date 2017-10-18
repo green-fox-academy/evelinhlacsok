d9cbebcb3a59570666bd9d5b5cbaf5a835a9baec
evi <evelinhlacsok@gmail.com>
2017-10-18 11:43:59 +0200

    evennumbers w lambda works as well

--- week06/day03/Exercise1-EvenNumbersArray/Exercise1-EvenNumbersArray/Program.cs
+++ week06/day03/Exercise1-EvenNumbersArray/Exercise1-EvenNumbersArray/Program.cs
@@ -14,18 +14,24 @@ namespace Exercise1_EvenNumbersArray
 
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
-            foreach (var evens in nQuery(n))
+            foreach (var evens in nLambda(n))
             {
             Console.WriteLine(evens);
             }
             Console.ReadLine();
         }
-        private static IEnumerable<int> nQuery (int[] n)
+        //private static IEnumerable<int> nQuery (int[] n)          //this works well as well, but line 17 needs to be updated
+        //{
+        //    IEnumerable<int> nQuery = from even in n
+        //                              where even % 2 == 0
+        //                              select even;
+        //    return nQuery;
+        //}
+
+        private static IEnumerable<int> nLambda (int[] n)
         {
-            IEnumerable<int> nQuery = from even in n
-                                      where even % 2 == 0
-                                      select even;
-            return nQuery;
+            IEnumerable<int> evens = n.Where(y => y % 2 == 0);
+            return evens;
         }
     }
 }
