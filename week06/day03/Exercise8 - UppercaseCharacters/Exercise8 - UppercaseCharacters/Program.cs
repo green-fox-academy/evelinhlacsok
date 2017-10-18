85ce3e7fa5682393dc52bde2cbd0d608bc52c98b
evi <evelinhlacsok@gmail.com>
2017-10-18 16:24:41 +0200

    uppercase characters with query and lambda

---
+++ week06/day03/Exercise8 - UppercaseCharacters/Exercise8 - UppercaseCharacters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8___UppercaseCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string input = "Most Is A Succulents Lesz";
+
+            var uppercaseWithQuery = (from uppercase in input
+                                      where char.IsUpper(uppercase)
+                                      select uppercase).ToList();
+
+            foreach (var letter in uppercaseWithQuery)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine();
+
+            var uppercaseWithLambda = input.Where(x => char.IsUpper(x)).ToList();
+            foreach (var letter in uppercaseWithLambda)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.ReadLine();
+
+
+            //var uppercaseWithLambda = input.Select(x => (Exists(x)) ? x.ToUpper() : x);
+
+        }
+    }
+}
