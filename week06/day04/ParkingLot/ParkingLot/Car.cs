4f4949efe3340d329b2ae5cdb326d6734dea240c
evi <evelinhlacsok@gmail.com>
2017-10-19 12:07:17 +0200

    parking lot, enums and first steps

---
+++ week06/day04/ParkingLot/ParkingLot/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public enum Type
+    {
+        Honda,
+        Toyota,
+        Volvo,
+        Audi
+    }
+
+    public enum Color
+    {
+        Black,
+        Red,
+        Blue
+    }
+
+    class Car
+    {
+        private Type type;
+        public Type Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+        private Color color;
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public Car(Type type, Color color)
+        {
+            this.type = type;
+            this.color = color;
+        }
+    }
+}
---
+++ week06/day04/ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int carNumber = 256;
+            List<Car> carList = new List<Car>();
+
+            for (int i = 1; i <= carNumber; i++)
+            {
+                 carList.Add(new Car(Type.Volvo, Color.Black));
+            }
+
+
+
+            foreach (var car in carList)
+            {
+                 Console.WriteLine(carList);
+            }
+            Console.ReadLine();
+        }
+    }
+}
