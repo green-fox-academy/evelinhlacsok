b3eed7256341ea0904e2db39e7899d44de1071ac
evi <evelinhlacsok@gmail.com>
2017-10-24 14:56:57 +0200

    hello word and hello world rest app

---
+++ week07/day02/workshop/workshop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace workshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("index")]
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public IActionResult Hello()
+        {
+            return Content("Hello World!");
+        }
+
+        [Route("everyone")]
+        public IActionResult Everyone()
+        {
+            return Content("Hello Everyone!");
+        }
+    }
+}
---
+++ week07/day02/workshop/workshop/Models/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace workshop.Models
+{
+    public class Greeting
+    {
+		public long Id { get; set; }
+		public string Content { get; set; }
+
+        public Greeting(long id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+    }
+}
