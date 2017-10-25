bce2547209904743c2e8b296afe3fb11ccbca5f7
evi <evelinhlacsok@gmail.com>
2017-10-25 11:48:40 +0200

    bank account class created

---
+++ week07/day03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+		public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount()
+        {
+
+        }
+    }
+}
