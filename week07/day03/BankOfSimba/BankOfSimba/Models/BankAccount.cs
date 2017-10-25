d2d47178a66a05081df25c0de1ba15c017f97b6a
evi <evelinhlacsok@gmail.com>
2017-10-25 15:19:18 +0200

    saving kings

--- week07/day03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
+++ week07/day03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -6,16 +6,18 @@ namespace BankOfSimba.Models
         public string Name { get; set; }
         public string Balance { get; set; }
         public AnimalType AnimalType { get; set; }
+        public bool IsKing { get; set; }
 
         public BankAccount()
         {
 
         }
-        public BankAccount(string name, string balance, AnimalType animalType)
+        public BankAccount(string name, string balance, AnimalType animalType, bool isKing)
         {
             Name = name;
             Balance = balance + "0.00" + " Zebra";
             AnimalType = AnimalType;
+            IsKing = isKing;
         }
     }
 }
