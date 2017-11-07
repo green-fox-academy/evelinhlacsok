c40d77877cb16071080a0d2ef7718ce96dd79bf9
evi <evelinhlacsok@gmail.com>
2017-11-07 13:36:00 +0100

    greeter works

--- week09/day02/ApiWorkshop/ApiWorkshop/Controllers/HomeController.cs
+++ week09/day02/ApiWorkshop/ApiWorkshop/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace ApiWorkshop.Models
     [Route("")]
     public class HomeController : Controller
     {
-        
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -28,6 +28,22 @@ namespace ApiWorkshop.Models
                 return Json(new { error = "Please provide an input!" });
             }
             return Json(new { received = input, result = input * 2 });
+        }
+
+        [HttpGet]
+        [Route("greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            else
+            return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
         }
     }
 }
