f1d20ce6779298ff2c29cecbc7cb5514c451ec60
evi <evelinhlacsok@gmail.com>
2017-11-07 11:28:12 +0100

    js and html file added

---
+++ week09/day02/ApiWorkshop/ApiWorkshop/Models/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiWorkshop.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiWorkshop.Models
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
