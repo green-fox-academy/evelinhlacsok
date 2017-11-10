3eb95e9c4c7469e73fcb04b5cb7b86a4084364a7
evi <evelinhlacsok@gmail.com>
2017-11-10 11:25:11 +0100

    bootstrap on add function

--- week09/day02/ApiWorkshop/IntegrationTesting/RestEndpointShould.cs
+++ week09/day02/ApiWorkshop/IntegrationTesting/RestEndpointShould.cs
@@ -4,23 +4,63 @@ using ApiWorkshop;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Xunit;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace IntegrationTesting
 {
-    public class UnitTest1
+    public class RestEndpointShould
     {
         private TestServer Server;
         private HttpClient Client;
 
-        public UnitTest1()
+        public RestEndpointShould()
         {
             Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             Client = Server.CreateClient();
         }
         [Fact]
-        public void Test1()
+        public async Task ReturnOkStatusDoubling()
         {
+            var response = await Client.GetAsync("/doubling");
 
+           Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Return30to15Doubling()
+        {
+            var response = await Client.GetAsync("doubling?input=15");
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("{\"received\":15,\"result\":30}", responseJson);
+        }
+
+        [Fact]
+        public async Task NoInputDoubling()
+        {
+            var response = await Client.GetAsync("doubling");
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("{\"error\":\"Please provide an input!\"}", responseJson);
+        }
+
+        [Fact]
+        public async Task ReturnOkStatusGreeter()
+        {
+            var response = await Client.GetAsync("/greeter");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReturnOkStatusArray()
+        {
+            var response = await Client.GetAsync("/array");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
