eef0127942374ef829c8b3084019be1f4db2e979
evi <evelinhlacsok@gmail.com>
2017-11-14 13:44:29 +0100

    file structure

---
+++ week10/FoxManager/FoxManager/Entities/FoxManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FoxManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoxManager.Entities
+{
+    public class FoxManagerContext : DbContext
+    {
+        public FoxManagerContext(DbContextOptions<FoxManagerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
